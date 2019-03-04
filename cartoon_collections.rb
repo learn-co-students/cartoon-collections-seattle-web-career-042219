ad7d80315721e4ee51698dda5baa9682ec24addc
Milan <milan.dhar@gmail.com>
2019-03-04 14:26:20 -0800

    methods

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,37 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names)# code an argument here
   # Your code here
+  names.each_with_index do |value, index|
+    puts "#{index+1}. #{value}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(calls)# code an argument here
   # Your code here
+  calls.collect do |names|
+    names = "#{names.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(words)# code an argument here
   # Your code here
+  words.any? do |call|
+    call.length > 4
+  end
+
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(foods)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  i = 0
+  while i < foods.length
+    #while i < foods.length
+    if cheese_types.include?(foods[i]) == true
+      return foods[i]
+      break
+    end
+      #i+= 1
+    i += 1
+  end
+
 end
